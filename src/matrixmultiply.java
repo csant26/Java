8aa47ab522f870151666aa937f4b5a46fb9dc3e6
csant09 <shishantstha@gmail.com>
2024-08-16 07:12:52 +0545

    commit

--- src/matrixmultiply.java
+++ src/matrixmultiply.java
@@ -2,6 +2,16 @@ import java.util.Scanner;
 
 public class matrixmultiply {
     public static void main(String[] args) throws Exception{
-        Scanner sc = new Scanner(System.in);
+        int[][] matrix = new int[][] {{1,2,3},{4,5,6,7},{8,9}};
+        for(int i = 0;i<matrix.length;i++)//row
+        {
+            int sum = 0;
+            for(int j = 0;j<matrix[i].length;j++)//column
+            {
+                //System.out.println(matrix[i][j]+" ");
+                sum+=matrix[i][j];
+            }
+            System.out.println("Sum of row"+(i+1)+":"+sum);
+        }
     }
 }
