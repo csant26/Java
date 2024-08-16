0e478ef934428f214bea28303b9f74d8310b9968
csant09 <shishantstha@gmail.com>
2024-08-16 07:36:05 +0545

    commit

---
+++ src/funcarr.java
@@ -0,0 +1,5 @@
+// Write a function that takes an array of integers as an argument and returns a value based on the sums of the even and odd numbers in the array. Let x= sum of odd numbers and y= sum of even numbers. function should return x y.
+
+public class funcarr {
+    
+}
--- src/jaggedarray.java
+++ src/jaggedarray.java
@@ -1,6 +1,4 @@
-import java.util.Scanner;
-
-public class matrixmultiply {
+public class jaggedarray {
     public static void main(String[] args) throws Exception{
         int[][] matrix = new int[][] {{1,2,3},{4,5,6,7},{8,9}};
         for(int i = 0;i<matrix.length;i++)//row
@@ -8,10 +6,10 @@ public class matrixmultiply {
             int sum = 0;
             for(int j = 0;j<matrix[i].length;j++)//column
             {
-                //System.out.println(matrix[i][j]+" ");
+                System.out.print(matrix[i][j]+" ");
                 sum+=matrix[i][j];
             }
-            System.out.println("Sum of row"+(i+1)+":"+sum);
+            System.out.println("Sum of row"+(i+1)+": "+sum);
         }
     }
 }
