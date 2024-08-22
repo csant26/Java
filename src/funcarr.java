254edd10b74942a12c1ed5360ea26751c5162ea4
csant26 <shishantstha@gmail.com>
2024-08-22 08:23:00 +0545

    commit

--- src/funcarr.java
+++ src/funcarr.java
@@ -1,5 +1,19 @@
-// Write a function that takes an array of integers as an argument and returns a value based on the sums of the even and odd numbers in the array. Let x= sum of odd numbers and y= sum of even numbers. function should return x y.
+// Write a function that takes an array of integers as an argument and returns a value based on the sums of the even and odd numbers in the array. Let x= sum of odd numbers and y= sum of even numbers. function should return x-y.
 
 public class funcarr {
-    
+    public static int OddEven(int[] arr){
+        int sumOdd = 0, sumEven=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2==0){
+                sumEven = sumEven + arr[i];
+            }else{
+                sumOdd = sumOdd + arr[i];
+            }
+        }
+        return (sumEven-sumOdd);
+    }
+    public static void main(String[] args) throws Exception{
+        System.out.println(OddEven(new int[] {1,2,3,4,5}));
+    }
 }
+
