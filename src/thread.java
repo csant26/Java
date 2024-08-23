1c668ceb18e2f176859f30a8d7b0f4eeef50a6f7
csant26 <shishantstha@gmail.com>
2024-08-23 07:06:26 +0545

    commit

---
+++ src/thread.java
@@ -0,0 +1,21 @@
+
+//Two methods to use thread.
+
+public class thread {
+    public static void main(String[] args) throws Exception{
+        //Method 1: Use anonymous class (inner/outer) to instantiate the thread.
+        Thread thread = new Thread(){
+            public void run(){
+                System.out.println("Thread is running.");
+            }
+        };
+        thread.start();
+    }
+}
+
+//Method 2: Esari extend garera tesko object through use garna ni milchha.
+// class MyThread extends Thread{
+//     public void run(){
+//         System.out.println("Thread is running.");
+//     }
+// }
