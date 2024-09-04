3f6b9d471fb01cb846f9037dfca29c6fb8db16ee
csant26 <shishantstha@gmail.com>
2024-09-04 19:56:42 +0545

    commit

---
+++ src/Radio_Button.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Radio_Button {
+    public static void main(String[] main) throws Exception{
+        JFrame f = new JFrame();
+        JRadioButton r1 = new JRadioButton("A) Male",true);
+        JRadioButton r2 = new JRadioButton("B) Female");
+        r1.setBounds(75,50,100,30);
+        r2.setBounds(75,100,100,30);
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(r1);
+        bg.add(r2);
+        f.add(r1);
+        f.add(r2);
+        f.setSize(300,300);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+}
