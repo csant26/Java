3a0fc030a98fc95f96c01ae1ac9fba4e9c424d95
csant26 <shishantstha@gmail.com>
2024-09-11 06:58:24 +0545

    commit

---
+++ src/Menu_Item.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class Menu_Item {
+    public static void main(String[] args) throws Exception{
+        JMenu menu, submenu;
+        JMenuItem i1,i2,i3,i4,i5;
+        JFrame f = new JFrame("Menu and MenuItem Example.");
+        JMenuBar mb = new JMenuBar();
+        menu = new JMenu("Menu");
+        submenu = new JMenu("Sub Menu");
+        i1 = new JMenuItem("item 1");
+        i2 = new JMenuItem("item 2");
+        i3 = new JMenuItem("item 3");
+        i4 = new JMenuItem("item 4");
+        i5 = new JMenuItem("item 5");
+        menu.add(i1);menu.add(i2);menu.add(i3);
+        submenu.add(i4);submenu.add(i5);
+        menu.add(submenu);
+        mb.add(menu);
+        f.setJMenuBar(mb);
+        f.setSize(400,400);
+        f.setLayout(null);
+        f.setVisible(true);
+    }    
+}
