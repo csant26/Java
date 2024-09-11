0241af77b302ced838fa2f328800f0a5e9dd0248
csant26 <shishantstha@gmail.com>
2024-09-11 07:05:44 +0545

    commit

---
+++ src/Pop_Menu.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Pop_Menu {
+    public static void main(String[] args) throws Exception{
+        JFrame f = new JFrame("PopMenu Example");
+        JPopupMenu popupmenu = new JPopupMenu("Edit");
+        JMenuItem cut = new JMenuItem("Cut");
+        JMenuItem copy = new JMenuItem("Copy");
+        JMenuItem paste = new JMenuItem("Paste");
+        popupmenu.add(cut);popupmenu.add(copy);popupmenu.add(paste);
+        f.addmenu
+    }
+}
