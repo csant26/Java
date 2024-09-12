fb31169ce8f0602a3c414ea9e6f486d8a76e0df9
csant26 <shishantstha@gmail.com>
2024-09-12 08:56:05 +0545

    commit

---
+++ src/Key_Listener.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+import java.awt.event.*;
+public class Key_Listener implements KeyListener {
+    Label l;
+    TextArea area;
+    Key_Listener(){
+        Frame f = new Frame();
+        l = new Label();
+        l.setBounds(20,50,100,20);
+        area = new TextArea();
+        area.setBounds(20,89,150,150);
+        area.addKeyListener(this);
+        f.add(l);f.add(area);
+        f.setSize(300,300);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        l.setText("Key Typed.");
+    }
+    @Override
+    public void keyPressed(KeyEvent e) {
+        l.setText("Key Pressed");
+    }
+    @Override
+    public void keyReleased(KeyEvent e) {
+        l.setText("Key Released.");
+    }
+    public static void main(String[] args) throws Exception{
+        new Key_Listener();
+    }
+}
