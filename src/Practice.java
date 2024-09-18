420e7358cdce04a5f387a25075a9f6aceef2c658
csant26 <shishantstha@gmail.com>
2024-09-18 07:55:15 +0545

    commit

---
+++ src/Practice.java
@@ -0,0 +1,14 @@
+import java.sql.*;
+
+public class Practice {
+    public static void main(String[] args) throws Exception{
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/javadb?useSSL=false", "root", "root");
+        Statement statement = con.createStatement();
+        ResultSet rs = statement.executeQuery("select * from tblstudent");
+        while(rs.next()){
+            System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
+        }
+        con.close();
+    }
+}
