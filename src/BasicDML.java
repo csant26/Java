354b4f1826c88cc3964a9c9c5f289ccff56dadf3
csant26 <shishantstha@gmail.com>
2024-09-18 08:14:18 +0545

    commit

--- src/BasicDML.java
+++ src/BasicDML.java
@@ -1,6 +1,6 @@
 import java.sql.*;
 
-public class Practice {
+public class BasicDML {
     public static void main(String[] args) throws Exception{
         Class.forName("com.mysql.jdbc.Driver");
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/javadb?useSSL=false", "root", "root");
