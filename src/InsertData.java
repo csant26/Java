3f45b0cae78ae11fa723af50dbfd25e2adcc748a
csant26 <shishantstha@gmail.com>
2024-09-20 07:19:44 +0545

    Insert Data

---
+++ src/InsertData.java
@@ -0,0 +1,16 @@
+import java.sql.*;
+
+public class InsertData {
+    public static void main(String[] args) throws Exception{
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/javadb?useSSL=false", "root", "root");
+        PreparedStatement ps = con.prepareStatement("insert into tblstudent(Id,Name,Address,Gender) values(?,?,?,?)");
+        ps.setInt(1, 4);
+        ps.setString(2, "Shishant");
+        ps.setString(3,"Hetauda");
+        ps.setString(4, "Male");
+        ps.executeUpdate();
+        System.out.println("Data Inserted.");
+        con.close();
+    }
+}
--- src/RetrieveData.java
+++ src/RetrieveData.java
@@ -1,6 +1,6 @@
 import java.sql.*;
 
-public class BasicDML {
+public class RetrieveData {
     public static void main(String[] args) throws Exception{
         Class.forName("com.mysql.jdbc.Driver");
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/javadb?useSSL=false", "root", "root");
