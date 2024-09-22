da40e570eafc034439e3a4fa8ebe813a39aa8270
csant26 <shishantstha@gmail.com>
2024-09-22 11:19:05 +0545

    send mail using java, area of radius in clent-server

--- src/InetAddressExample.java
+++ src/InetAddressExample.java
@@ -1,7 +1,12 @@
+//Factory Method : creates socket instance; getByName(), getLocalHost() etc. are used to get InetAddress obj
+//Getter Method : retrieves properties of a socket instance; getHostName(), getHostAddress() etc. are used to retrieve information about the object.
+
 import java.net.*;
 public class InetAddressExample {
     public static void main(String[] args) throws Exception{
         InetAddress address = InetAddress.getByName("www.google.com");
-        System.out.println(address.getHostAddress());
+        System.out.println(address);
+        InetAddress localhost = InetAddress.getLocalHost();
+        System.out.println(localhost);
     }
 }
---
+++ src/SourceCode_URL.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.net.*;
+
+public class SourceCode_URL {
+    public static void main(String[] args) throws Exception {
+        try{    
+            URL url=new URL("http://www.samriddhicollege.com/jbsccsit-college-in-bhaktapur-nepal");    
+            URLConnection urlcon=url.openConnection();    
+            InputStream stream=urlcon.getInputStream();    
+            int i;    
+            while((i=stream.read())!=-1){    
+            System.out.print((char)i);    
+            }    
+            }catch(Exception e){System.out.println(e);}    
+            
+    }
+
+}
